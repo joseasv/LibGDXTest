427f0bed19e39067e822ed455743c2be9e8bd017
José Sánchez <joseasv@outlook.com>
2018-07-12 15:36:44 -0400

    Cambiando la forma en que se interceptan las balas y enemigos. El juego ahora corre mucho mejor.

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -4,52 +4,33 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.ModelLoader;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
-import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.*;
-import com.badlogic.gdx.math.collision.BoundingBox;
-import com.badlogic.gdx.math.collision.Sphere;
 import com.badlogic.gdx.utils.Array;
-import com.sun.javafx.geom.transform.Identity;
 
-import javax.jws.WebParam;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Texture texBala;
-	Sprite spriteNave;
-	Vector2 velNave, acNave;
-	Sprite spriteEnemigo;
-	List<Sprite> listaBalas;
+	private SpriteBatch batch;
+	private Texture img;
+	private Texture texBala;
+	private Sprite spriteNave;
+	private Vector2 velNave, acNave;
+	private Sprite spriteEnemigo;
+
 	private float x = 0;
 	private ModelBatch modelBatch;
 	private Environment environment;
 	private AssetManager assets;
 	private boolean loading;
-	private Array<ModelInstance> instances = new Array<ModelInstance>();
-	private Array<ModelInstance> balaInstances = new Array<ModelInstance>();
 	private Array<ModelInstance> escenarioIns = new Array<ModelInstance>();
-	private Array<Sphere> balaSphere = new Array<Sphere>();
 	private Array<Sprite> listaBalas2 = new Array<Sprite>();
 
-	private CameraInputController camController;
-
-	private Model bala3D;
-	private ModelInstance enemigo3DIns;
-	private boolean eneActivo;
-	private BoundingBox eneBB;
 	private PerspectiveCamera cam2;
-	private ModelInstance sceneInstance;
 
 	@Override
 	public void create () {
@@ -57,10 +38,8 @@ public class Main extends ApplicationAdapter {
 		img = new Texture("nave.png");
 		spriteNave = new Sprite(img);
 		spriteNave.setPosition(200,200);
-		//spriteNave.scale(2);
 		velNave = new Vector2(400,400);
 		acNave = new Vector2();
-
 
 		Texture texEnemigo = new Texture("enemigo1.png");
 		spriteEnemigo = new Sprite(texEnemigo);
@@ -68,18 +47,12 @@ public class Main extends ApplicationAdapter {
 		spriteEnemigo.scale(2);
 
 		texBala = new Texture("bala1.png");
-		listaBalas = new ArrayList<Sprite>();
 
 		modelBatch = new ModelBatch();
 
 		environment = new Environment();
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
 		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
-
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-
-
 
 		cam2 = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		cam2.position.set(0f, 5f, 3f);
@@ -88,12 +61,9 @@ public class Main extends ApplicationAdapter {
 		cam2.far = 300f;
 		cam2.update();
 
-
-
 		assets = new AssetManager();
 		assets.load("escena.g3db", Model.class);
 		loading = true;
-
 
 	}
 
@@ -101,14 +71,11 @@ public class Main extends ApplicationAdapter {
 
 
 		Model scene = assets.get("escena.g3db", Model.class);
-		sceneInstance = new ModelInstance(scene);
+		ModelInstance sceneInstance = new ModelInstance(scene);
 		sceneInstance.transform.translate(20,0, -5);
 		sceneInstance.transform.rotate(0,1,0,90);
 
-
 		escenarioIns.add(sceneInstance);
-		//instances.add(sceneInstance);
-		
 		loading = false;
 	}
 
@@ -116,7 +83,6 @@ public class Main extends ApplicationAdapter {
 	public void render () {
 		if (loading && assets.update())
 			doneLoading();
-		//camController.update();
 
 		//Gdx.gl.glClearColor(0.2F, 0, 0, 1);
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -143,7 +109,6 @@ public class Main extends ApplicationAdapter {
 			Sprite spriteBala = new Sprite(texBala);
 			spriteBala.scale(2);
 			spriteBala.setPosition(spriteNave.getBoundingRectangle().getX() + spriteNave.getWidth() / 2, spriteNave.getBoundingRectangle().getY() + spriteNave.getHeight() / 2 );
-			//listaBalas.add(spriteBala);
 			listaBalas2.add(spriteBala);
 		}
 
@@ -162,25 +127,19 @@ public class Main extends ApplicationAdapter {
 				bala.translateX(25);
 				bala.draw(batch);
 
-				if (spriteEnemigo.getBoundingRectangle().contains(bala.getBoundingRectangle())){
+				if (Intersector.overlaps(spriteEnemigo.getBoundingRectangle(), bala.getBoundingRectangle())){
 					spriteEnemigo.setAlpha(0);
 				}
 			}
 		}
 
 		spriteNave.draw(batch);
-		if (spriteEnemigo != null){
+
 			spriteEnemigo.draw(batch);
-		}
+
 
 		batch.end();
-
-
-
-	}
-
-	private void checkColission(){
-
+		
 	}
 	
 	@Override
@@ -188,9 +147,7 @@ public class Main extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 		modelBatch.dispose();
-		instances.clear();
 		escenarioIns.clear();
-		balaInstances.clear();
 		assets.dispose();
 	}
 }
